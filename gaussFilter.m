4bd041570484f9980a04de6fcfc742decb926e7c
Elias Datler <eliasdatler@me.com>
2019-11-04 15:12:22 +0100

    Add filter classes

---
+++ gaussFilter.m
@@ -0,0 +1,23 @@
+classdef GaussFilter
+    %GaussFilter Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Property1
+    end
+    
+    methods
+        function obj = untitled(inputArg1,inputArg2)
+            %GaussFilter Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
+
