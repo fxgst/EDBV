80c14b9916bcccad9c88658fc70a4430b80c4db0
Elias Datler <eliasdatler@me.com>
2020-01-05 15:31:56 +0100

    Add own binary image implementation

--- Filter/BinaryImage.m
+++ Filter/BinaryImage.m
@@ -1,15 +1,28 @@
+% Author: Elias Datler 11775795
 classdef BinaryImage
-    properties
-        Property1 % not used
-    end
-    
     methods(Static)
         
+		% matlab implementation
         function outputImage = Binary(image)
             gray = rgb2gray(image);
             outputImage = imbinarize(gray, 'adaptive');
             %outputImage = edge(gray);
-        end
+		end
+		
+		% own implementation
+		function binImg = Binary_implementation(Image, params)
+
+			if ~isfield(params,'threshold'); params.threshold = 0.01; end
+
+			binImg = Image;
+
+			% black or white depending on the threshold
+			binImg(Image <= params.threshold) = 0;
+			binImg(Image > params.threshold) = 256;
+
+			%convert to logical image
+			binImg = logical(binImg);
+		end
         
     end
 end
